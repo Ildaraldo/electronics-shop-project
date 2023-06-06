b32e0549d5ff7ef5abb6881e2782cdc1cf11e446
programmer <ildarinho99@gmail.com>
2023-06-06 16:19:44 +0500

    homework-1 -> реализация класса Item в src/item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src import item
+
+item1 = item.Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price():
+    """
+    Тестирование функции 'calculate_total_price' класса 'Item'"""
+    assert item1.calculate_total_price() == 200_000
+
+
+def test_apply_discount():
+    """
+    Тестирование функции 'apply_discount' класса 'Item'"""
+    item1.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8_000
+
