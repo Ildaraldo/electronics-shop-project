868d1cb4755fc93d231abe149ca1f2e33b190b79
programmer <ildarinho99@gmail.com>
2023-07-13 00:35:53 +0500

    homework-5 -> Создан класс Keyboard для товара “клавиатура”.

--- src/item.py
+++ src/item.py
@@ -21,6 +21,8 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
+        super().__init__()
+
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
